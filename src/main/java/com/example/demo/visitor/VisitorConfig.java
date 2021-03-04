c7d8419dc30a5126f37a91ad8a4a3a0391372193
iuianturcanu <iudturcanu@gmail.com>
2021-03-04 08:31:28 -0500

    tests still not working

--- src/main/java/com/example/demo/visitor/VisitorConfig.java
+++ src/main/java/com/example/demo/visitor/VisitorConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 public class VisitorConfig {
--- src/test/java/com/example/demo/visitor/VisitorTest.java
+++ src/test/java/com/example/demo/visitor/VisitorTest.java
@@ -1,14 +1,23 @@
 package com.example.demo.visitor;
 
+import com.example.demo.VisitorJpaConfig;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import javax.annotation.Resource;
+import javax.transaction.Transactional;
 
 @SpringBootTest
+@ContextConfiguration(
+        classes = {VisitorJpaConfig.class },
+        loader = AnnotationConfigContextLoader.class)
+@Transactional
 public class VisitorTest {
 
-    @Autowired
+    @Resource
     VisitorRepository visitorRepository;
 
     @Test
