a85d6db0e0b4efbc51c60c04309a69afb343375c
iuianturcanu <iudturcanu@gmail.com>
2021-03-11 17:55:40 -0500

    InventoryDto added

--- src/main/java/com/iudtu/lunchbox/dto/InventoryDto.java
+++ src/main/java/com/iudtu/lunchbox/dto/InventoryDto.java
@@ -1,9 +1,15 @@
 package com.iudtu.lunchbox.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(value = JsonInclude.Include.NON_NULL)
 public class InventoryDto {
-    private Long count;
-    private ItemType itemType;
+
+    private Long id;
+    private String name;
+    private Integer count;
 }
