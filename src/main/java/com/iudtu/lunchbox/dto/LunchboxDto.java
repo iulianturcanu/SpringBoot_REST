01751abd11f4a4211fec9d8f84ad303b3cdbf878
iuianturcanu <iudturcanu@gmail.com>
2021-03-10 22:13:50 -0500

    LunchboxDto first review

--- src/main/java/com/iudtu/lunchbox/dto/LunchboxDto.java
+++ src/main/java/com/iudtu/lunchbox/dto/LunchboxDto.java
@@ -1,8 +1,17 @@
 package com.iudtu.lunchbox.dto;
 
-import lombok.Data;
+import lombok.*;
+
+import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class LunchboxDto {
-    private String name;
+    private String owner;
+    private String description;
+    private Integer capacity;
+    private List<LunchboxItemDto> lunchboxItemsList;
 }
