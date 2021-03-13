77fbfcde1ad48783e6fae93d6d2a4db92f3b46b7
iuianturcanu <iudturcanu@gmail.com>
2021-03-12 19:15:17 -0500

    LunchboxItem some work done

--- src/main/java/com/iudtu/lunchbox/repository/LunchboxItemRepository.java
+++ src/main/java/com/iudtu/lunchbox/repository/LunchboxItemRepository.java
@@ -1,7 +1,16 @@
 package com.iudtu.lunchbox.repository;
 
+import com.iudtu.lunchbox.dto.ItemType;
 import com.iudtu.lunchbox.model.LunchboxItem;
+import com.sun.tools.javac.code.Attribute;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Iterator;
+import java.util.Optional;
+
 public interface LunchboxItemRepository extends CrudRepository<LunchboxItem, Long> {
+
+    Optional<LunchboxItem> findByName(String name);
+    Iterator<LunchboxItem> findByItemType(ItemType itemType);
+
 }
